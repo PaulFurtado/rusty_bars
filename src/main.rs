e105350fd7253577fd2c9019b44493a6da07f56b
Paul Furtado <paulfurtado91@gmail.com>
2015-04-13 15:11:46 -0400

    document main

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate rust_pulse;
 use rust_pulse::pulse::PulseAudioMainloop;
 use rust_pulse::viz_runner::VizRunner;
 
-
+/// Start the visualizer for your default PulseAudio output.
 fn main() {
     let mainloop = PulseAudioMainloop::new();
     VizRunner::new(&mainloop);
