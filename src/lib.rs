6d741488f47850e9893ecbdbfcd545a85be825d9
Paul Furtado <paulfurtado91@gmail.com>
2015-04-13 15:10:16 -0400

    document rust allowances

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(unstable)]
-#![feature(unsafe_destructor)]
-#![allow(unstable_features)]
+#![feature(unsafe_destructor)] // For destructing parameterized types
+#![allow(unstable_features)] // To suppress the warning of unsafe_destructor
 
 pub mod ncurses_wrapper;
 pub mod pulse;
